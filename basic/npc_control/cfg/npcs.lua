9219fa5c9feb8f9911b2b37e5a6d34d16fb819f4
Demo <51279632+Demo4889@users.noreply.github.com>
2020-05-06 22:21:01 -0400

    Update npcs.lua
    Added all NPC's that would have weapons. Also added all police cars that I know of.

--- basic/npc_control/cfg/npcs.lua
+++ basic/npc_control/cfg/npcs.lua
@@ -20,7 +20,7 @@ cfg = {}
 
 cfg.density = {
 	peds = 1.0,
-	vehicles = 1.0
+	vehicles = 0.5
 }
 
 cfg.peds = { -- these peds wont show up anywhere, they will be removed and their vehicles deleted
@@ -30,19 +30,113 @@ cfg.peds = { -- these peds wont show up anywhere, they will be removed and their
   "s_m_y_hwaycop_01",
   "s_m_y_swat_01",
   "s_m_m_snowcop_01",
-  "s_m_m_paramedic_01"
+  "s_m_m_paramedic_01",
+  "s_m_y_fireman_01",
+  "s_m_y_pilot_01"
 }
 
 cfg.noguns = { -- these peds wont have any weapons
+  "g_f_importexport_01",
+  "g_f_importexport_01",
+  "g_f_y_ballas_01",
+  "g_f_y_families_01",
+  "g_f_y_lost_01",
+  "g_f_y_vagos_01",
+  "g_m_importexport_01",
+  "g_m_m_armboss_01",
+  "g_m_m_armgoon_01",
+  "g_m_m_armlieut_01",
+  "g_m_m_chemwork_01",
+  "g_m_m_chiboss_01",
+  "g_m_m_chicold_01",
+  "g_m_m_chigoon_01",
+  "g_m_m_chigoon_02",
+  "g_m_m_korboss_01",
+  "g_m_m_mexboss_01",
+  "g_m_m_mexboss_02",
+  "g_m_y_armgoon_02",
+  "g_m_y_azteca_01",
+  "g_m_y_ballaeast_01",
+  "g_m_y_ballaorig_01",
+  "g_m_y_ballasout_01",
+  "g_m_y_famca_01",
+  "g_m_y_famdnf_01",
+  "g_m_y_famfor_01",
+  "g_m_y_korean_01",
+  "g_m_y_korean_02",
+  "g_m_y_korlieut_01",
+  "g_m_y_lost_01",
+  "g_m_y_lost_02",
+  "g_m_y_lost_03",
+  "g_m_y_mexgang_01",
+  "g_m_y_mexgoon_01",
+  "g_m_y_mexgoon_02",
+  "g_m_y_mexgoon_03",
+  "g_m_y_pologoon_01",
+  "g_m_y_pologoon_02",
+  "g_m_y_salvaboss_01",
+  "g_m_y_salvagoon_01",
+  "g_m_y_salvagoon_02",
+  "g_m_y_salvagoon_03",
+  "g_m_y_strpunk_01",
+  "g_m_y_strpunk_02",
+  "g_m_m_casrn_01"
 }
 
 cfg.nodrops = { -- these peds wont drop their weapons when killed
+"g_f_importexport_01",
+"g_f_importexport_01",
+"g_f_y_ballas_01",
+"g_f_y_families_01",
+"g_f_y_lost_01",
+"g_f_y_vagos_01",
+"g_m_importexport_01",
+"g_m_m_armboss_01",
+"g_m_m_armgoon_01",
+"g_m_m_armlieut_01",
+"g_m_m_chemwork_01",
+"g_m_m_chiboss_01",
+"g_m_m_chicold_01",
+"g_m_m_chigoon_01",
+"g_m_m_chigoon_02",
+"g_m_m_korboss_01",
+"g_m_m_mexboss_01",
+"g_m_m_mexboss_02",
+"g_m_y_armgoon_02",
+"g_m_y_azteca_01",
+"g_m_y_ballaeast_01",
+"g_m_y_ballaorig_01",
+"g_m_y_ballasout_01",
+"g_m_y_famca_01",
+"g_m_y_famdnf_01",
+"g_m_y_famfor_01",
+"g_m_y_korean_01",
+"g_m_y_korean_02",
+"g_m_y_korlieut_01",
+"g_m_y_lost_01",
+"g_m_y_lost_02",
+"g_m_y_lost_03",
+"g_m_y_mexgang_01",
+"g_m_y_mexgoon_01",
+"g_m_y_mexgoon_02",
+"g_m_y_mexgoon_03",
+"g_m_y_pologoon_01",
+"g_m_y_pologoon_02",
+"g_m_y_salvaboss_01",
+"g_m_y_salvagoon_01",
+"g_m_y_salvagoon_02",
+"g_m_y_salvagoon_03",
+"g_m_y_strpunk_01",
+"g_m_y_strpunk_02",
+"g_m_m_casrn_01"
 }
 
 
---[[ WORK IN PROGRESS // DOES NOT WORK
+--WORK IN PROGRESS // DOES NOT WORK
 cfg.vehs = { -- these vehicles wont show up anywhere, they will be removed unless a player is in the driver seat
   "police",
+  "police2",
+  "police3",
   "policet",
   "sheriff",
   "fbi",
@@ -50,4 +144,3 @@ cfg.vehs = { -- these vehicles wont show up anywhere, they will be removed unles
   "riot",
   "pbus"
 }
-]]
